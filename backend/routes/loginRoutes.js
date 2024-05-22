159ad7f5d985834251ced7568c791ea0333a4816
ricualex <alexandruricu25@gmail.com>
2024-05-22 12:32:03 +0300

    CodeReview: Remove unused vault code

--- backend/routes/loginRoutes.js
+++ backend/routes/loginRoutes.js
@@ -17,37 +17,3 @@ module.exports = function (router) {
         }
     });
 }
-
-
-// var options = {
-//     apiVersion: 'v1',
-//     endpoint: 'http://127.0.0.1:8200',
-//     token: 'MY_TOKEN'
-//   };
-//   var vault = require("node-vault")(options);
-//   vault.init({ secret_shares: 1, secret_threshold: 1 })
-//   .then( (result) => {
-//     var keys = result.keys;
-//     vault.token = result.root_token;
-//     return vault.unseal({ secret_shares: 1, key: keys[0] })
-//   })
-//   .catch(console.error);
-
-// const vault = require('node-vault')({ endpoint: 'http://127.0.0.1:8200', token: 'your-root-token' });
-
-
-// vault.write('secret/hello', { value: 'world', lease: '1s' })
-// .then( () => vault.read('secret/hello'))
-// .then( () => vault.delete('secret/hello'))
-// .catch(console.error);
-
-// async function storeSecret() {
-//   await vault.write('/data/exchangeRates', {});
-// }
-
-// async function getSecret() {
-//   const result = await vault.read('secret/data/myapp');
-//   console.log(result.data.data);
-// }
-
-// storeSecret().then(getSecret);
