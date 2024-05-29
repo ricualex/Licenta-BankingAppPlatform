345d31e88fd5090b05b8425b742884030a38e325
ricualex <alexandruricu25@gmail.com>
2024-05-29 23:17:10 +0300

    Style left menu

--- frontend/src/views/ibanking/components/InternetBankingMenu.jsx
+++ frontend/src/views/ibanking/components/InternetBankingMenu.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import "../InternetBankingStyle.css"
 import { useNavigate } from "react-router-dom";
+import Button from '@mui/material/Button';
+import HomeIcon from '@mui/icons-material/Home';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import SwapHorizIcon from '@mui/icons-material/SwapHoriz';
+import ScheduleIcon from '@mui/icons-material/Schedule';
+import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
+import InstallMobileIcon from '@mui/icons-material/InstallMobile';
+import HistoryIcon from '@mui/icons-material/History';
+import KeyIcon from '@mui/icons-material/Key';
 
 const InternetBankingMenu = ({ setSelectedTab }) => {
 
@@ -14,55 +23,80 @@ const InternetBankingMenu = ({ setSelectedTab }) => {
         navigate("/home");
     }
 
+    const menuButtonStyle = {
+        color: 'white',
+        fontSize: "1vw",
+        borderBottom: "3px solid rgb(3, 0, 28)",
+        justifyContent: "start",
+        allignItems: "start",
+        marginLeft: "10px",
+        textTransform: "none",
+        '& .MuiButton-startIcon': {
+            color: 'white',
+        },
+        '&:hover': {
+            backgroundColor: "rgb(3, 0, 28)",
+        },
+    }
+
     return (
         <div className="left-menu normal-panel">
-            <button
-                className="left-menu-button"
+            <Button className="left-menu-button"
+                startIcon={<HomeIcon />}
                 onClick={goToHome}
+                sx={menuButtonStyle}
             >
                 Home
-            </button>
-            <button
-                className="left-menu-button"
+            </Button>
+            <Button className="left-menu-button"
+                startIcon={<AccountBalanceWalletIcon />}
                 onClick={() => handleTabSelection("account")}
+                sx={menuButtonStyle}
             >
                 My Account
-            </button>
-            <button
-                className="left-menu-button"
-                onClick={() => handleTabSelection("transfer")}>
+            </Button>
+            <Button className="left-menu-button"
+                startIcon={<SwapHorizIcon />}
+                onClick={() => handleTabSelection("transfer")}
+                sx={menuButtonStyle}
+            >
                 Transfer money
-            </button>
-            <button
-                className="left-menu-button"
+            </Button>
+            <Button className="left-menu-button"
+                startIcon={<ScheduleIcon />}
                 onClick={() => handleTabSelection("payments")}
+                sx={menuButtonStyle}
             >
                 Payments scheduler
-            </button>
-            <button
-                className="left-menu-button"
+            </Button>
+            <Button className="left-menu-button"
+                startIcon={<CurrencyExchangeIcon />}
                 onClick={() => handleTabSelection("exchange")}
+                sx={menuButtonStyle}
             >
                 Exchange
-            </button>
-            <button
-                className="left-menu-button"
+            </Button>
+            <Button className="left-menu-button"
+                startIcon={<HistoryIcon />}
                 onClick={() => handleTabSelection("transactions")}
+                sx={menuButtonStyle}
             >
                 Transactions
-            </button>
-            <button
-                className="left-menu-button"
+            </Button>
+            <Button className="left-menu-button"
+                startIcon={<InstallMobileIcon />}
                 onClick={() => handleTabSelection("mobile_banking")}
+                sx={menuButtonStyle}
             >
                 Mobile banking
-            </button>
-            <button
-                className="left-menu-button"
+            </Button>
+            <Button className="left-menu-button"
+                startIcon={<KeyIcon />}
                 onClick={() => handleTabSelection("change_password")}
+                sx={menuButtonStyle}
             >
                 Change password
-            </button>
+            </Button>
         </div>
     )
 }
