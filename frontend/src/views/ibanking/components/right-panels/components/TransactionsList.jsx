c1a30dfd747839efd4302383134c9697bc30be6b
ricualex <alexandruricu25@gmail.com>
2024-06-22 13:19:43 +0300

    Login testing

--- frontend/src/views/ibanking/components/right-panels/components/TransactionsList.jsx
+++ frontend/src/views/ibanking/components/right-panels/components/TransactionsList.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Grid, List, ListItem, ListItemText, Typography } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import "./styles.css";
 
 const TransactionList = () => {
     const [transactions, setTransactions] = useState([]);
@@ -25,17 +28,19 @@ const TransactionList = () => {
             <Grid container spacing={3} style={{ marginTop: "40px" }}>
                 <Grid item xs={12} md={6}>
                     <Typography variant="h5" gutterBottom style={{ color: 'white', marginLeft: "40px" }}>
-                        My Transactions
+                        My Payments
                     </Typography>
-                    <List sx={{ maxHeight: 400, overflow: 'auto', marginLeft: "40px" }}>
+                    <List className='transactions-list' sx={{ maxHeight: 400, overflow: 'auto', marginLeft: "40px" }}>
                         {transactions.map(transaction => (
-                            <ListItem key={transaction.id}>
+                            <ListItem key={transaction.id} dense style={{ display: 'flex', alignItems: 'center' }}>
                                 <ListItemText
                                     primary={`Destination: ${transaction.destinationName}`}
-                                    secondary={`Amount: ${transaction.amount}`}
+                                    secondary={`Amount: ${transaction.amount} lei`}
                                     primaryTypographyProps={{ style: { color: 'white' } }}
                                     secondaryTypographyProps={{ style: { color: 'white' } }}
+                                    style={{ flex: '0 1 auto', width: "250px" }}
                                 />
+                                <RemoveIcon sx={{ color: 'red' }} />
                             </ListItem>
                         ))}
                     </List>
@@ -44,15 +49,19 @@ const TransactionList = () => {
                     <Typography variant="h5" gutterBottom style={{ color: 'white' }}>
                         Friends Transactions
                     </Typography>
-                    <List sx={{ maxHeight: 400, overflow: 'auto' }}>
+                    <List className='transactions-list' sx={{ maxHeight: "400px", overflow: 'auto' }}>
                         {friendsTransactions.map((transaction, index) => (
-                            <ListItem key={index}>
+                            <ListItem key={index} dense style={{ display: 'flex', alignItems: 'center' }}>
                                 <ListItemText
                                     primary={`Friend: ${transaction.userName}`}
-                                    secondary={`Amount: ${Object.values(transaction.amount)[0]}`}
+                                    secondary={`Amount: ${Object.values(transaction.amount)[0]} lei`}
                                     primaryTypographyProps={{ style: { color: 'white' } }}
                                     secondaryTypographyProps={{ style: { color: 'white' } }}
+                                    style={{ flex: '0 1 auto', width: "150px" }}
                                 />
+                                {Object.values(transaction.amount)[0] > 0 ?
+                                    <AddIcon sx={{ color: 'green', marginLeft: "30px" }} /> :
+                                    <RemoveIcon sx={{ color: 'red', marginLeft: "30px" }} />}
                             </ListItem>
                         ))}
                     </List>
