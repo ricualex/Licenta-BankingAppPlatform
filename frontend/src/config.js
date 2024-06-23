5915dc39e10f835609654662295fc2689af7ee07
ricualex <alexandruricu25@gmail.com>
2024-06-23 14:37:59 +0300

    Test deploy without ports

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,16 +1,16 @@
 const config = {
-    exchangeRatesApi: 'http://localhost:8080/api/exchangeRatesAll',
-    fetchSchedulerApi: 'http://localhost:8081/fetch-scheduler',
-    syncUserApi: 'http://localhost:8080/api/syncUser',
-    transferMoneyApi: 'http://localhost:8080/api/transferMoney',
-    getFriendsNamesApi: 'http://localhost:8080/api/getFriendNames',
-    schedulePaymentApi: 'http://localhost:8081/schedule-payment',
-    fetchSchedulerApi: 'http://localhost:8081/fetch-scheduler',
-    deleteScheduleApi: 'http://localhost:8081/delete-scheduler',
-    getUserTransactionsApi: 'http://localhost:8080/api/getUserTransactions?userName=user',
-    getExchangeRatesApi: 'http://localhost:8080/api/getExchangeRate',
-    loginApi: 'http://localhost:8080/api/login',
-    convertCurrencyApi: 'http://localhost:8080/api/convertCurrency'
+    exchangeRatesApi: 'http://localhost/api/exchangeRatesAll',
+    fetchSchedulerApi: 'http://localhost/fetch-scheduler',
+    syncUserApi: 'http://localhost/api/syncUser',
+    transferMoneyApi: 'http://localhost/api/transferMoney',
+    getFriendsNamesApi: 'http://localhost/api/getFriendNames',
+    schedulePaymentApi: 'http://localhost/schedule-payment',
+    fetchSchedulerApi: 'http://localhost/fetch-scheduler',
+    deleteScheduleApi: 'http://localhost/delete-scheduler',
+    getUserTransactionsApi: 'http://localhost/api/getUserTransactions?userName=user',
+    getExchangeRatesApi: 'http://localhost/api/getExchangeRate',
+    loginApi: 'http://localhost/api/login',
+    convertCurrencyApi: 'http://localhost/api/convertCurrency'
   };
   
   export default config;
