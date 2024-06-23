8556e537a5308fc673f2680577650164e65499c4
ricualex <alexandruricu25@gmail.com>
2024-06-23 13:09:06 +0300

    Add redis connection for docker configs

--- backend/routes/redisRoutes.js
+++ backend/routes/redisRoutes.js
@@ -1,10 +1,14 @@
 const redis = require('redis');
 const redisClient = redis.createClient({
-  url: 'redis://127.0.0.1:6379'
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
 });
 
 redisClient.on('error', (err) => {
   console.error('Error connecting to Redis:', err);
+});
+
+redisClient.on('connect', () => {
+  console.log('Connected to Redis');
 });
 
 redisClient.connect();
